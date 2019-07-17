771dd4a263b2e42f91d17f5724b94e00e7bc81ad
timochfati <48986815+timochfati@users.noreply.github.com>
2019-07-17 04:50:58 +0200

    Add files via upload

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+    {
+    'id':1,
+    'title': 'Article one',
+    'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'author': 'fatima lamrini',
+    'create_date':'16-07-2019'
+    },
+        {
+        'id':2,
+        'title': 'Article two',
+        'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'author': 'adam lamrini',
+        'create_date':'16-07-2019'
+        },
+            {
+            'id':3,
+            'title': 'Article three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'kenza lamrini',
+            'create_date':'16-07-2019'
+            }
+
+    ]
+    return articles
